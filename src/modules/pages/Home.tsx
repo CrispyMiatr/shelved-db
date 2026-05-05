4ba43bd12a7f6a41baa00889cef8ca827a2aaa12
CrispyMiatr <maxivowes@gmail.com>
2026-05-05 22:10:24 +0200

    feat(home): add basic HTML structure + icon scss file

--- src/modules/pages/Home.tsx
+++ src/modules/pages/Home.tsx
@@ -1,9 +1,92 @@
-import '~styles/pages/home.scss'
+import home from '~styles/pages/home.module.scss'
+import icon from '~styles/components/icons.module.scss'
+import banner from '~assets/banner.jpg'
+import chevron from '~assets/chevron-left.svg'
 
 export const Home = () => {
     return (
-        <div className='home-container'>
-            <h1>Home</h1>
+        <div className={home['home-container']}>
+            <div className={home['hero']}>
+                <div className={home['hero__text']}>
+                    <div className={home['hero__text__title']}>
+                        Shelved.
+                    </div>
+
+                    <div className={home['hero__text__subtitle']}>
+                        The Ultimate Beverage Database
+                    </div>
+
+                    <div className={home['hero__text__description']}>
+                        All beverages worldwide for everyone to see!
+                    </div>
+                </div>
+
+                <img
+                    className={home['hero__banner']}
+                    src={banner}
+                    alt="a wall of old cans"
+                />
+            </div>
+
+            <div className={home['released']}>
+                <div className={home['released__header']}>
+                    <div className={home['released__header__title']}>
+                        <h3>Newly Released</h3>
+                    </div>
+
+                    <div className={home['released__header__timestamp']}>
+                        <img src="" alt="clock icon"/>
+                        <p>Next refresh in: 16:55:42</p>
+                    </div>
+                </div>
+                <div className={home['released__carousel']}>
+                    <img src={chevron} alt="left" className={icon['chevron']}/>
+
+                    <div className={home['released__carousel__items']}>
+                        Temp.
+                    </div>
+
+                    <img src={chevron} alt="right" className={`${icon['chevron']} ${icon['chevron--right']}`}/>
+                </div>
+
+            </div>
+
+            <div className={home['added']}>
+                <div className={home['added__header']}>
+                    <div className={home['added__header__title']}>
+                        <h3>Newly Added</h3>
+                    </div>
+
+                    <div className={home['added__header__timestamp']}>
+                        <img src="" alt="clock icon"/>
+                        <p>Next refresh in: 56 s</p>
+                    </div>
+                </div>
+                <div className={home['added__carousel']}>
+                    <img src={chevron} alt="left" className={icon['chevron']}/>
+
+                    <div className={home['added__carousel__items']}>
+                        Temp.
+                    </div>
+
+                    <img src={chevron} alt="right" className={`${icon['chevron']} ${icon['chevron--right']}`}/>
+                </div>
+            </div>
+
+            <div className={home['profiles']}>
+                    <div className={home['profiles__title']}>
+                        <h3>Popular Profiles</h3>
+                    </div>
+                <div className={home['profiles__carousel']}>
+                    <img src={chevron} alt="left" className={icon['chevron']}/>
+
+                    <div className={home['profiles__carousel__items']}>
+                        Temp.
+                    </div>
+
+                    <img src={chevron} alt="right" className={`${icon['chevron']} ${icon['chevron--right']}`}/>
+                </div>
+            </div>
         </div>
     );
 };
