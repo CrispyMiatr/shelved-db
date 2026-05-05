daf3e3091161be29507a105606527c8ea6a27ac1
CrispyMiatr <maxivowes@gmail.com>
2026-05-05 22:27:15 +0200

    feat(catalogue): add basic HTML structure

--- src/modules/pages/Catalogue.tsx
+++ src/modules/pages/Catalogue.tsx
@@ -1,9 +1,49 @@
 import '~styles/pages/catalogue.scss'
+import home from "~styles/pages/home.module.scss";
+import chevron from "~assets/chevron-left.svg";
+import icon from "~styles/components/icons.module.scss";
 
 export const Catalogue = () => {
     return (
         <div className='catalogue-container'>
-            <h1>Catalogue</h1>
+            <h2>Catalogue</h2>
+
+            <div className={home['brands']}>
+                <div className={home['brands__title']}>
+                    <h3>Popular Profiles</h3>
+                </div>
+                <div className={home['brands__carousel']}>
+                    <img src={chevron} alt="left" className={icon['chevron']}/>
+
+                    <div className={home['brands__carousel__items']}>
+                        Temp.
+                    </div>
+
+                    <img src={chevron} alt="right" className={`${icon['chevron']} ${icon['chevron--right']}`}/>
+                </div>
+            </div>
+
+            <div className={home['index']}>
+                <div className={home['index__header']}>
+                    <div className={home['index__header__title']}>
+                        <h3>All brands</h3>
+                    </div>
+
+                    <div className={home['index__header__filter']}>
+                        <div className={home['index-group']}>
+                            <div className={home['index-group__item']}>
+                                <div className={home['index-group__item__title']}>
+                                    3D Energy Drink
+                                </div>
+
+                                <div className={home['index-group__item__count']}>
+                                    [53]
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
