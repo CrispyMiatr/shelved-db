4a6317e19e8d4ebba1356d9eda42faf56bb6e3b9
CrispyMiatr <maxivowes@gmail.com>
2026-05-06 16:31:51 +0200

    feat(brand): add basic HTML structure + basic scss

--- src/modules/pages/Brand.tsx
+++ src/modules/pages/Brand.tsx
@@ -1,9 +1,36 @@
-import '~styles/pages/brand.scss'
+import brand from '~styles/pages/brand.module.scss';
+import CocaCola from '~assets/logo_coca-cola.png'
 
 export const Brand = () => {
     return (
-        <div className='brand-container'>
-            <h1>Brand</h1>
+        <div className={brand['brand-container']}>
+            <div className={brand['breadcrumbs']}>
+                <p>Catalogue • Brand Name</p>
+            </div>
+
+            <div className={brand['header']}>
+                <div className={brand['header__brand-title']}>
+                    <h2>The</h2>
+                    <span className={brand['divider-v']}></span>
+                    <img src={CocaCola} alt="brand logo" className={brand['header__brand-title__image']} />
+                    <span className={brand['divider-v']}></span>
+                    <h2>Shelf</h2>
+                </div>
+
+                <div className={brand['header__filter']}>
+                    <p>filter dropdowns</p>
+                </div>
+            </div>
+
+            <span className={brand['divider-h']}></span>
+
+            <div className={brand['products']}>
+                <div className={brand['products__sort']}>
+                    <p>sort buttons</p>
+                </div>
+
+                <p>products</p>
+            </div>
         </div>
     );
 };
