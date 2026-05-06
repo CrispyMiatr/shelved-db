f34fda5fd002c69d761794dc58c3b8ce3e5fbeec
CrispyMiatr <maxivowes@gmail.com>
2026-05-06 18:01:08 +0200

    feat(profile): add basic HTML structure + basic scss

--- src/modules/pages/Profile.tsx
+++ src/modules/pages/Profile.tsx
@@ -1,9 +1,57 @@
-import '~styles/pages/profile.scss'
+import profile from '~styles/pages/profile.module.scss';
 
 export const Profile = () => {
     return (
-        <div className='profile-container'>
-            <h1>Profile</h1>
+        <div className={profile['profile-container']}>
+            <h2>Profile</h2>
+
+            <div className={profile['info-wrap']}>
+                <div className={profile['info-wrap__avatar']}>
+                    <img src="" alt="avatar"/>
+                </div>
+
+                <div className={profile['info-wrap__info']}>
+                    <div className={profile['info-wrap__info__name']}>
+
+                    </div>
+
+                    <div className={profile['info-wrap__info__bio']}>
+
+                    </div>
+
+                    <div className={profile['info-wrap__info__data']}>
+
+                    </div>
+
+                    <div className={profile['info-wrap__info__socials']}>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className={profile['collection-wrap']}>
+                <div className={profile['collection-wrap__header']}>
+                    <h3>My Collection</h3>
+
+                    <div className={profile['collection-wrap__header__buttons']}>
+
+                    </div>
+
+                    <div className={profile['collection-wrap__header__filter']}>
+                        <p>filter dropdowns</p>
+                    </div>
+                </div>
+
+                <span className={profile['divider-h']}></span>
+
+                <div className={profile['collection-wrap__products']}>
+                    <div className={profile['collection-wrap__products__sort']}>
+                        <p>sort buttons</p>
+                    </div>
+
+                    <p>products</p>
+                </div>
+            </div>
         </div>
     );
 };
