8fa0e91584915e04211ca79af59241065486637c
Maxim Westerbeek <maxivowes@gmail.com>
2026-05-16 19:48:31 +0200

    feat(Footer): make basic footer

--- resources/js/components/layout/Footer.tsx
+++ resources/js/components/layout/Footer.tsx
@@ -1,9 +1,87 @@
-import '~styles/app.scss'
+import { Link, usePage } from '@inertiajs/react';
+import { NavButton } from '~/components';
+import { PageProps } from '~/types';
+import logo from "~assets/logo-white.svg"
+import footer from '~styles/components/layout/footer.module.scss'
 
 export const Footer = () => {
+    const { auth } = usePage<PageProps>().props;
+    const user = auth.user;
+
     return (
-        <nav className='footer'>
-            <h3>This is an unfinished footer</h3>
-        </nav>
+        <footer className={footer['footer']}>
+            <div className={footer['footer__container']}>
+                <div className={footer['footer__container__brand']}>
+                    <Link href="/">
+                        <img src={logo} alt="Shelved." className={footer['footer__container__brand__logo']} />
+                        <div className={footer['footer__container__brand__text']}>
+                            <h3>Shelved.</h3>
+                            <p>The Ultimate Beverage Database</p>
+                        </div>
+                    </Link>
+                </div>
+
+                <div className={footer['footer__container__socials']}>
+                    <h4>Social links</h4>
+                    <ul className={footer['links']}>
+                        <li className={footer['links__link-item']}>
+                            <a
+                                href="https://www.instagram.com/crispy.drinks"
+                                target='_blank'
+                                rel="noopener noreferrer">
+
+                                <img src="/assets/icons/icon_instagram-inv.svg" alt="instagram" />
+                                <p>@crispy.drinks</p>
+                            </a>
+                        </li>
+                        <li className={footer['links__link-item']}>
+                            <a
+                                href="https://bsky.app/profile/crispydrinks.bsky.social"
+                                target='_blank'
+                                rel="noopener noreferrer">
+
+                                <img src="/assets/icons/icon_bluesky-inv.svg" alt="bluesky" />
+                                <p>@crispydrinks.bsky.social</p>
+                            </a>
+                        </li>
+                        <li className={footer['links__link-item']}>
+                            <a
+                                href="mailto:crispy.drinks@shelvedb.com"
+                                target='_blank'
+                                rel="noopener noreferrer">
+
+                                <img src="/assets/icons/icon_email.svg" alt="email" />
+                                <p>crispy.drinks@shelvedb.com</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className={footer['footer__container__legal']}>
+                    <h4>Legal</h4>
+                    <ul className={footer['links']}>
+                        <li className={footer['links__link-item']}><a href="">Terms of service</a></li>
+                        <li className={footer['links__link-item']}><a href="">Cookie policy</a></li>
+                        <li className={footer['links__link-item']}><a href="">Privacy notice</a></li>
+                    </ul>
+                </div>
+
+                <div className={footer['footer__container__pages']}>
+                    <h4>Legal</h4>
+                    <ul className={footer['links']}>
+                        <li className={footer['links__link-item']}><NavButton name="Home" link="/" /></li>
+                        <li className={footer['links__link-item']}><NavButton name="Catalogue" link="/catalogue" /></li>
+                        <li className={footer['links__link-item']}><NavButton name="Collectors" link="/collectors" /></li>
+                        <li className={footer['links__link-item']}><NavButton
+                            name="Profile"
+                            link={user ? `/@${user.username}` : "/login"}
+                        /></li>
+                        {!user && (
+                            <li className={footer['links__link-item']}><NavButton name="Sign In" link="/login" /></li>
+                        )}
+                    </ul>
+                </div>
+            </div>
+        </footer>
     );
 };
--- resources/js/components/ui/Button.tsx
+++ resources/js/components/ui/Button.tsx
@@ -3,14 +3,13 @@ import type { ButtonItem } from '~/types/button.types';
 import '~styles/app.scss'
 
 export const NavButton = ({ name, link }: ButtonItem) => {
-    // Check if the current URL matches the link exactly, or starts with the link
     const { url } = usePage();
     const isActive = url === link || (link !== '/' && url.startsWith(link));
 
     return (
         <Link
             href={link}
-            className={isActive ? 'nav-button button--active' : 'nav-button'}
+            className={`nav-button ${isActive ? 'button--active' : ''}`}
         >
             <p className='nav-button__title'>{name}</p>
         </Link>
