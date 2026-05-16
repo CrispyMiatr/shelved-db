ce207ab54ab8c17789a2cb56bb7ddc57d63c30d0
Maxim Westerbeek <maxivowes@gmail.com>
2026-05-16 19:49:08 +0200

    feat(Header): edit structure + change style slightly

--- resources/js/components/layout/Header.tsx
+++ resources/js/components/layout/Header.tsx
@@ -2,31 +2,53 @@ import { Link, usePage } from '@inertiajs/react';
 import { NavButton } from "~/components";
 import { PageProps } from '~/types';
 import logo from "~assets/logo-black.svg"
-import '~styles/app.scss'
+import header from '~styles/components/layout/header.module.scss'
 
 export const Header = () => {
     const { auth } = usePage<PageProps>().props;
     const user = auth.user;
 
     return (
-        <nav className='navbar'>
-            <div className='navbar__container'>
-                <Link href="/">
-                    <img src={logo} alt="Shelved." className='navbar__container__logo' />
-                </Link>
+        <nav className={header['navbar']}>
+            <div className={header['navbar__container']}>
+                <div className={header['navbar__container__top']}>
 
-                <NavButton name="Home" link="/" />
-                <NavButton name="Catalogue" link="/catalogue" />
-                <NavButton name="Collectors" link="/collectors" />
+                </div>
 
-                <NavButton
-                    name="Profile"
-                    link={user ? `/@${user.username}` : "/login"}
-                />
+                <div className={header['navbar__container__bottom']}>
+                    <ul className={header['links']}>
+                        <li className={header['links__link-item']}>
+                            <Link href="/">
+                                <img src={logo} alt="Shelved." className={header['navbar__container__logo']} />
+                            </Link>
+                        </li>
 
-                {!user && (
-                    <NavButton name="Sign In" link="/login" />
-                )}
+                        <li className={header['links__link-item']}>
+                            <NavButton name="Home" link="/" />
+                        </li>
+
+                        <li className={header['links__link-item']}>
+                            <NavButton name="Catalogue" link="/catalogue" />
+                        </li>
+
+                        <li className={header['links__link-item']}>
+                            <NavButton name="Collectors" link="/collectors" />
+                        </li>
+
+                        <li className={header['links__link-item']}>
+                            <NavButton
+                                name="Profile"
+                                link={user ? `/@${user.username}` : "/login"}
+                            />
+                        </li>
+
+                        {!user && (
+                            <li className={header['links__link-item']}>
+                                <NavButton name="Sign In" link="/login" />
+                            </li>
+                        )}
+                    </ul>
+                </div>
             </div>
         </nav>
     );
