640569fef6d27ca9290614fbd86ebec212180d27
Maxim Westerbeek <maxivowes@gmail.com>
2026-05-23 17:12:38 +0200

    feat(Brand): add styling

--- resources/js/Pages/Brand.tsx
+++ resources/js/Pages/Brand.tsx
@@ -2,6 +2,7 @@ import { Breadcrumbs, FilterGroup, Layout, ProductCard, SortButton } from '~/com
 import { BeverageType, BrandType } from '~/types';
 import { Link } from '@inertiajs/react';
 import styles from '~styles/pages/brand.module.scss';
+import logo from '~assets/logo_coca-cola.png'
 
 interface Props {
     brand: BrandType;
@@ -28,24 +29,25 @@ const Brand = ({ brand, beverages, filters, options, sort }: Props) => {
 
             <div className={styles['header']}>
                 <div className={styles['header__brand-title']}>
-                    <h2>The</h2>
+                    <h3>The</h3>
                     <span className={styles['divider-v']}></span>
 
                     <img
-                        src={brand.logo_path || 'https://placehold.co/100x100?text=Brand Logo'}
+                        // src={brand.logo_path || 'https://placehold.co/100x100?text=Brand Logo'}
+                        src={logo}
                         alt={`${brand.name} logo`}
                         className={styles['header__brand-title__image']}
                     />
 
                     <span className={styles['divider-v']}></span>
-                    <h2>Shelf</h2>
+                    <h3>Shelf</h3>
                 </div>
 
                 <div className={styles['header__filter']}>
                     <FilterGroup filters={filters} options={options} />
-                    <p className={styles['item-count']}>
+                    {/* <p className={styles['item-count']}>
                         {beverages.length} {beverages.length === 1 ? 'Item' : 'Items'} found
-                    </p>
+                    </p> */}
                 </div>
             </div>
 
@@ -53,12 +55,11 @@ const Brand = ({ brand, beverages, filters, options, sort }: Props) => {
 
             <div className={styles['products']}>
                 <div className={styles['products__sort']}>
+                    <SortButton label="Added" field="created_at" currentSort={field} currentDirection={direction} />
                     <SortButton label="Name" field="name" currentSort={field} currentDirection={direction} />
                     <SortButton label="Country" field="country_code" currentSort={field} currentDirection={direction} />
                     <SortButton label="Year" field="release_date" currentSort={field} currentDirection={direction} />
                     <SortButton label="Volume" field="volume" currentSort={field} currentDirection={direction} />
-                    <SortButton label="Flavour" field="lineup_flavor" currentSort={field} currentDirection={direction} />
-                    <SortButton label="Newest" field="created_at" currentSort={field} currentDirection={direction} />
                 </div>
 
                 <div className={styles['products__grid']}>
@@ -69,7 +70,7 @@ const Brand = ({ brand, beverages, filters, options, sort }: Props) => {
                                 name={beverage.name}
                                 volume={beverage.volume}
                                 country={beverage.country_code}
-                                img={beverage.img_url || 'https://placehold.co/150x200'}
+                                img={beverage.img_url || 'https://placehold.co/130x160'}
                                 isSmall={true}
                                 href={`/catalogue/${brand.slug}/${beverage.slug}`}
                             />
