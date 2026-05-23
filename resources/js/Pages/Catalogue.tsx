35d04601ffe646ace4924a58c0cd0abfae106648
Maxim Westerbeek <maxivowes@gmail.com>
2026-05-23 18:55:35 +0200

    feat(Catalogue): temp replace BrandCard img

--- resources/js/Pages/Catalogue.tsx
+++ resources/js/Pages/Catalogue.tsx
@@ -45,7 +45,8 @@ const Catalogue = ({ brands, popularBrands }: CatalogueProps) => {
                                 key={brand.id}
                                 brand={brand.name}
                                 count={brand.beverages_count}
-                                img={brand.logo_path || 'https://placehold.co/200x200'}
+                                // img={brand.logo_path || 'https://placehold.co/200x200'}
+                                img={'https://placehold.co/200x200'}
                                 href={`/catalogue/${brand.slug}`}
                             />
                         ))}
