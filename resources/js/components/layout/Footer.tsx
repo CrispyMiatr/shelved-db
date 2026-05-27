efde4398666b1de8f53597eddb1d03a973b58bba
Maxim Westerbeek <maxivowes@gmail.com>
2026-05-27 06:47:10 +0200

    feat(Footer): stylise footer + change socials icons

--- resources/js/components/layout/Footer.tsx
+++ resources/js/components/layout/Footer.tsx
@@ -1,5 +1,4 @@
 import { Link, usePage } from '@inertiajs/react';
-import { NavButton } from '~/components';
 import { PageProps } from '~/types';
 import logo from "~assets/logo-white.svg"
 import footer from '~styles/components/layout/footer.module.scss'
@@ -11,77 +10,74 @@ export const Footer = () => {
     return (
         <footer className={footer['footer']}>
             <div className={footer['footer__container']}>
-                <div className={footer['footer__container__brand']}>
+
+                {/* Brand Section */}
+                <div className={footer['footer__brand']}>
                     <Link href="/">
-                        <img src={logo} alt="Shelved." className={footer['footer__container__brand__logo']} />
-                        <div className={footer['footer__container__brand__text']}>
+                        <img src={logo} alt="Shelved." className={footer['footer__brand__logo']} />
+                        <div className={footer['footer__brand__text']}>
                             <h3>Shelved.</h3>
-                            <p>The Ultimate Beverage Database</p>
+                            <p>The Ultimate <br />Beverage <br />Database</p>
                         </div>
                     </Link>
                 </div>
 
-                <div className={footer['footer__container__socials']}>
+                {/* Socials Section */}
+                <div className={footer['footer__socials']}>
                     <h4>Social links</h4>
                     <ul className={footer['links']}>
                         <li className={footer['links__link-item']}>
-                            <a
-                                href="https://www.instagram.com/crispy.drinks"
-                                target='_blank'
-                                rel="noopener noreferrer">
-
-                                <img src="/assets/icons/icon_instagram-inv.svg" alt="instagram" />
-                                <p>@crispy.drinks</p>
+                            <a href="https://www.instagram.com/crispy.drinks" target='_blank' rel="noreferrer">
+                                <img src="/assets/icons/icon_instagram-w.svg" alt="" />
+                                @crispy.drinks
                             </a>
                         </li>
                         <li className={footer['links__link-item']}>
-                            <a
-                                href="https://bsky.app/profile/crispydrinks.bsky.social"
-                                target='_blank'
-                                rel="noopener noreferrer">
-
-                                <img src="/assets/icons/icon_bluesky-inv.svg" alt="bluesky" />
-                                <p>@crispydrinks.bsky.social</p>
+                            <a href="https://bsky.app/profile/crispydrinks.bsky.social" target='_blank' rel="noreferrer">
+                                <img src="/assets/icons/icon_bluesky-w.svg" alt="" />
+                                @crispydrinks.bsky.social
                             </a>
                         </li>
                         <li className={footer['links__link-item']}>
-                            <a
-                                href="mailto:crispy.drinks@shelvedb.com"
-                                target='_blank'
-                                rel="noopener noreferrer">
-
-                                <img src="/assets/icons/icon_email.svg" alt="email" />
-                                <p>crispy.drinks@shelvedb.com</p>
+                            <a href="mailto:crispy.drinks@shelvedb.com">
+                                <img src="/assets/icons/icon_email-w.svg" alt="" />
+                                crispy.drinks@shelvedb.com
                             </a>
                         </li>
                     </ul>
                 </div>
 
-                <div className={footer['footer__container__legal']}>
+                {/* Legal Section */}
+                <div className={footer['footer__legal']}>
                     <h4>Legal</h4>
                     <ul className={footer['links']}>
-                        <li className={footer['links__link-item']}><a href="">Terms of service</a></li>
-                        <li className={footer['links__link-item']}><a href="">Cookie policy</a></li>
-                        <li className={footer['links__link-item']}><a href="">Privacy notice</a></li>
+                        <li className={footer['links__link-item']}><Link href="#">Terms of service</Link></li>
+                        <li className={footer['links__link-item']}><Link href="#">Cookie policy</Link></li>
+                        <li className={footer['links__link-item']}><Link href="#">Privacy notice</Link></li>
                     </ul>
                 </div>
 
-                <div className={footer['footer__container__pages']}>
-                    <h4>Legal</h4>
-                    <ul className={footer['links']}>
-                        <li className={footer['links__link-item']}><NavButton name="Home" link="/" /></li>
-                        <li className={footer['links__link-item']}><NavButton name="Catalogue" link="/catalogue" /></li>
-                        <li className={footer['links__link-item']}><NavButton name="Collectors" link="/collectors" /></li>
-                        <li className={footer['links__link-item']}><NavButton
-                            name="Profile"
-                            link={user ? `/@${user.username}` : "/login"}
-                        /></li>
-                        {!user && (
-                            <li className={footer['links__link-item']}><NavButton name="Sign In" link="/login" /></li>
-                        )}
+                {/* Navigation Section - Fixed Links */}
+                <div className={footer['footer__pages']}>
+                    <h4>Pages</h4>
+                    <ul className={footer['links-grid']}>
+                        <li className={footer['links__link-item']}><Link href="/">Home</Link></li>
+                        <li className={footer['links__link-item']}><Link href="/catalogue">Catalogue</Link></li>
+                        <li className={footer['links__link-item']}><Link href="/collectors">Collectors</Link></li>
+                        <li className={footer['links__link-item']}><Link href="/manufacturers">Manufacturers</Link></li>
+                        <li className={footer['links__link-item']}><Link href="/about">About</Link></li>
+                        <li className={footer['links__link-item']}>
+                            <Link href={user ? `/@${user.username}` : "/login"}>
+                                {user ? 'Profile' : 'Sign In'}
+                            </Link>
+                        </li>
                     </ul>
                 </div>
             </div>
+
+            <p className={footer['footer__copyright']}>
+                © 2026 Shelved.com
+            </p>
         </footer>
     );
 };
